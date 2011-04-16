5053743664cacdb46368dc0cca29ba49f20b0ecb
Dave Brosius <dbrosius@mebigfatguy.com>
2011-04-16 00:42:34 -0400

    cache bezier pts

--- src/com/mebigfatguy/borders4j/ScrollBorder.java
+++ src/com/mebigfatguy/borders4j/ScrollBorder.java
@@ -26,6 +26,8 @@ import java.awt.Insets;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.Stroke;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.border.AbstractBorder;
 
@@ -35,6 +37,8 @@ public class ScrollBorder extends AbstractBorder {
 
 	private final Options options;
 	private final Stroke stroke;
+	private Rectangle cacheBounds;
+	private final List<float[][]> bezierPts = new ArrayList<float[][]>();
 
 	public ScrollBorder() {
 		this(new Options());
@@ -69,109 +73,33 @@ public class ScrollBorder extends AbstractBorder {
 		RenderingHints saveHints = g2d.getRenderingHints();
 		try {
 			Rectangle r = c.getBounds();
+
+			if ((cacheBounds == null) || !r.equals(cacheBounds)) {
+				recalculateBezierPts(r);
+				cacheBounds = (Rectangle)r.clone();
+			}
+
 			g.setColor(options.color);
 			g2d.setStroke(stroke);
 			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
+			for (float[][] pts : bezierPts) {
+				BezierRenderer.draw(g, pts);
+			}
+
 			if (options.top > 0) {
-
-				float lx = r.x + (r.width / 5.0f);
-				float rx = r.x + ((r.width * 4) / 5.0f);
-
-				float[][] pts = new float[][]
-				{
-					{lx, r.y + options.top - options.lineWidth},
-					{lx - 10, r.y + options.top - options.lineWidth},
-					{lx - 10, r.y + 1},
-					{lx, r.y + 1}
-				};
-				BezierRenderer.draw(g, pts);
-				pts = new float[][]
-				{
-					{lx, r.y + 1},
-					{lx + 30, r.y + 1},
-					{rx - 30, r.y + options.top - options.lineWidth},
-					{rx, r.y + options.top - options.lineWidth}
-				};
-				BezierRenderer.draw(g, pts);
-				pts = new float[][]
-  				{
-  					{rx, r.y + options.top - options.lineWidth},
-  					{rx + 10, r.y + options.top - options.lineWidth},
-  					{rx + 10, r.y + 1},
-  					{rx, r.y + 1}
-  				};
-  				BezierRenderer.draw(g, pts);
-
 				int midx = r.x + r.width / 2;
   				g.drawLine(midx - 10, r.y + options.top - options.lineWidth, midx, r.y + options.lineWidth);
   				g.drawLine(midx, r.y + options.top - options.lineWidth, midx + 10, r.y + options.lineWidth);
 			}
 
 			if (options.left > 0) {
-				float ty = r.y + (r.height / 5.0f);
-				float by = r.y + ((r.height * 4) / 5.0f);
-
-				float[][] pts = new float[][]
-                {
-					{r.x + options.left - options.lineWidth, ty},
-					{r.x + options.left - options.lineWidth, ty - 10},
-					{r.x + options.lineWidth, ty - 10},
-					{r.x + options.lineWidth, ty}
-                };
-				BezierRenderer.draw(g, pts);
-				pts = new float[][]
-			    {
-					{r.x + options.lineWidth, ty},
-					{r.x + options.lineWidth, ty + 30},
-					{r.x + options.left - options.lineWidth, by - 30},
-					{r.x + options.left - options.lineWidth, by}
-			    };
-				BezierRenderer.draw(g, pts);
-				pts = new float[][]
-  			    {
-					{r.x + options.left - options.lineWidth, by},
-					{r.x + options.left - options.lineWidth, by + 10},
-					{r.x + options.lineWidth, by + 10},
-  					{r.x + options.lineWidth, by}
-  			    };
-  				BezierRenderer.draw(g, pts);
-
 				int midy = r.y + r.height / 2;
   				g.drawLine(r.x + options.lineWidth, midy - 10, r.x + options.left - options.lineWidth, midy);
   				g.drawLine(r.x + options.lineWidth, midy, r.x + options.left - options.lineWidth, midy + 10);
 			}
 
 			if (options.bottom > 0) {
-
-				float lx = r.x + (r.width / 5.0f);
-				float rx = r.x + ((r.width * 4) / 5.0f);
-
-				float[][] pts = new float[][]
-				{
-					{lx, r.y + r.height - options.lineWidth},
-					{lx - 10, r.y + r.height - options.lineWidth},
-					{lx - 10, r.y + r.height - options.bottom + options.lineWidth},
-					{lx, r.y + r.height - options.bottom + options.lineWidth}
-				};
-				BezierRenderer.draw(g, pts);
-				pts = new float[][]
-				{
-					{lx, r.y + r.height - options.bottom + options.lineWidth},
-					{lx + 30, r.y + r.height - options.bottom + options.lineWidth},
-					{rx - 30, r.y + r.height - options.lineWidth},
-					{rx, r.y + r.height - options.lineWidth}
-				};
-				BezierRenderer.draw(g, pts);
-				pts = new float[][]
-  				{
-					{rx, r.y + r.height - options.lineWidth},
-					{rx + 10, r.y + r.height - options.lineWidth},
-					{rx + 10, r.y + r.height - options.bottom + options.lineWidth},
-					{rx, r.y + r.height - options.bottom + options.lineWidth},
-  				};
-  				BezierRenderer.draw(g, pts);
-
 				int midx = r.x + r.width / 2;
   				g.drawLine(midx - 10, r.y + r.height - options.lineWidth, midx, r.y + r.height - options.bottom + options.lineWidth);
   				g.drawLine(midx, r.y + r.height - options.lineWidth, midx + 10, r.y + r.height - options.bottom + options.lineWidth);
@@ -179,44 +107,149 @@ public class ScrollBorder extends AbstractBorder {
 			}
 
 			if (options.right > 0) {
-				float ty = r.y + (r.height / 5.0f);
-				float by = r.y + ((r.height * 4) / 5.0f);
-
-				float[][] pts = new float[][]
-                {
-					{r.x + r.width - options.right + options.lineWidth, ty},
-					{r.x + r.width - options.right + options.lineWidth, ty - 10},
-					{r.x + r.width - options.lineWidth, ty - 10},
-					{r.x + r.width - options.lineWidth, ty}
-                };
-				BezierRenderer.draw(g, pts);
-				pts = new float[][]
-			    {
-					{r.x + r.width - options.lineWidth, ty},
-					{r.x + r.width - options.lineWidth, ty + 30},
-					{r.x + r.width - options.right + options.lineWidth, by - 30},
-					{r.x + r.width - options.right + options.lineWidth, by}
-			    };
-				BezierRenderer.draw(g, pts);
-				pts = new float[][]
-  			    {
-					{r.x + r.width - options.right + options.lineWidth, by},
-					{r.x + r.width - options.right + options.lineWidth, by + 10},
-					{r.x + r.width - options.lineWidth, by + 10},
-  					{r.x + r.width - options.lineWidth, by}
-  			    };
-  				BezierRenderer.draw(g, pts);
-
 				int midy = r.y + r.height / 2;
   				g.drawLine(r.x + r.width - options.right - options.lineWidth, midy - 10, r.x + r.width - options.lineWidth, midy);
   				g.drawLine(r.x + r.width - options.right - options.lineWidth, midy, r.x + r.width - options.lineWidth, midy + 10);
-
 			}
 
 		} finally {
 			g.setColor(saveColor);
 			g2d.setStroke(saveStroke);
 			g2d.setRenderingHints(saveHints);
+		}
+	}
+
+	private void recalculateBezierPts(Rectangle r) {
+		bezierPts.clear();
+
+		if (options.top > 0) {
+
+			float lx = r.x + (r.width / 5.0f);
+			float rx = r.x + ((r.width * 4) / 5.0f);
+
+			float[][] pts = new float[][]
+			{
+				{lx, r.y + options.top - options.lineWidth},
+				{lx - 10, r.y + options.top - options.lineWidth},
+				{lx - 10, r.y + 1},
+				{lx, r.y + 1}
+			};
+			bezierPts.add(pts);
+
+			pts = new float[][]
+			{
+				{lx, r.y + 1},
+				{lx + 30, r.y + 1},
+				{rx - 30, r.y + options.top - options.lineWidth},
+				{rx, r.y + options.top - options.lineWidth}
+			};
+			bezierPts.add(pts);
+
+			pts = new float[][]
+			{
+				{rx, r.y + options.top - options.lineWidth},
+				{rx + 10, r.y + options.top - options.lineWidth},
+				{rx + 10, r.y + 1},
+				{rx, r.y + 1}
+			};
+			bezierPts.add(pts);
+		}
+
+		if (options.left > 0) {
+			float ty = r.y + (r.height / 5.0f);
+			float by = r.y + ((r.height * 4) / 5.0f);
+
+			float[][] pts = new float[][]
+            {
+				{r.x + options.left - options.lineWidth, ty},
+				{r.x + options.left - options.lineWidth, ty - 10},
+				{r.x + options.lineWidth, ty - 10},
+				{r.x + options.lineWidth, ty}
+            };
+			bezierPts.add(pts);
+
+			pts = new float[][]
+		    {
+				{r.x + options.lineWidth, ty},
+				{r.x + options.lineWidth, ty + 30},
+				{r.x + options.left - options.lineWidth, by - 30},
+				{r.x + options.left - options.lineWidth, by}
+		    };
+			bezierPts.add(pts);
+
+			pts = new float[][]
+			{
+				{r.x + options.left - options.lineWidth, by},
+				{r.x + options.left - options.lineWidth, by + 10},
+				{r.x + options.lineWidth, by + 10},
+					{r.x + options.lineWidth, by}
+			};
+			bezierPts.add(pts);
+		}
+
+		if (options.bottom > 0) {
+
+			float lx = r.x + (r.width / 5.0f);
+			float rx = r.x + ((r.width * 4) / 5.0f);
+
+			float[][] pts = new float[][]
+			{
+				{lx, r.y + r.height - options.lineWidth},
+				{lx - 10, r.y + r.height - options.lineWidth},
+				{lx - 10, r.y + r.height - options.bottom + options.lineWidth},
+				{lx, r.y + r.height - options.bottom + options.lineWidth}
+			};
+			bezierPts.add(pts);
+
+			pts = new float[][]
+			{
+				{lx, r.y + r.height - options.bottom + options.lineWidth},
+				{lx + 30, r.y + r.height - options.bottom + options.lineWidth},
+				{rx - 30, r.y + r.height - options.lineWidth},
+				{rx, r.y + r.height - options.lineWidth}
+			};
+			bezierPts.add(pts);
+
+			pts = new float[][]
+			{
+				{rx, r.y + r.height - options.lineWidth},
+				{rx + 10, r.y + r.height - options.lineWidth},
+				{rx + 10, r.y + r.height - options.bottom + options.lineWidth},
+				{rx, r.y + r.height - options.bottom + options.lineWidth},
+			};
+			bezierPts.add(pts);
+		}
+
+		if (options.right > 0) {
+			float ty = r.y + (r.height / 5.0f);
+			float by = r.y + ((r.height * 4) / 5.0f);
+
+			float[][] pts = new float[][]
+            {
+				{r.x + r.width - options.right + options.lineWidth, ty},
+				{r.x + r.width - options.right + options.lineWidth, ty - 10},
+				{r.x + r.width - options.lineWidth, ty - 10},
+				{r.x + r.width - options.lineWidth, ty}
+            };
+			bezierPts.add(pts);
+
+			pts = new float[][]
+		    {
+				{r.x + r.width - options.lineWidth, ty},
+				{r.x + r.width - options.lineWidth, ty + 30},
+				{r.x + r.width - options.right + options.lineWidth, by - 30},
+				{r.x + r.width - options.right + options.lineWidth, by}
+		    };
+			bezierPts.add(pts);
+
+			pts = new float[][]
+			{
+				{r.x + r.width - options.right + options.lineWidth, by},
+				{r.x + r.width - options.right + options.lineWidth, by + 10},
+				{r.x + r.width - options.lineWidth, by + 10},
+				{r.x + r.width - options.lineWidth, by}
+			};
+			bezierPts.add(pts);
 		}
 	}
 

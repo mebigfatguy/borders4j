20de3a74021cd59b6513b91abf2c817dced54b9d
Dave Brosius <dbrosius@mebigfatguy.com>
2011-04-16 21:59:49 -0400

    add license

--- src/com/mebigfatguy/borders4j/ScallopBorder.java
+++ src/com/mebigfatguy/borders4j/ScallopBorder.java
@@ -1,3 +1,20 @@
+/*
+ * borders4j - An collection of swing borders
+ * Copyright 2011 MeBigFatGuy.com
+ * Copyright 2011 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package com.mebigfatguy.borders4j;
 
 import java.awt.BasicStroke;
@@ -18,6 +35,8 @@ import javax.swing.border.AbstractBorder;
 import com.mebigfatguy.borders4j.bezier.BezierRenderer;
 
 public class ScallopBorder extends AbstractBorder {
+
+	private static final long serialVersionUID = 155555265161268134L;
 
 	private final Options options;
 	private final Stroke stroke;
